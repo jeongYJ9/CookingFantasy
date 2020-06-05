fb1a43135086b6f4ae27f209f5b39edd4388a800
Lavumi <lavumi@gmail.com>
2020-06-06 01:37:57 +0900

    build controller

---
+++ Assets/buildingController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingController : MonoBehaviour
+{
+
+    const float minMoveDist = 3;
+    const float xFlipSnap = 30;
+
+    Vector3 inputStartPos;
+    int isXrot;
+    bool startTouch;
+    float dragStartRotY;
+    Vector3 dragStartPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void StartDrag(){
+        startTouch = true;
+        dragStartRotY = transform.eulerAngles.y;
+        dragStartPos = transform.position;
+        isXrot = 0;
+        inputStartPos = Input.mousePosition;
+    }
+
+    void EndDrag(){
+       // inputStartPos = new Vector3(0);
+       startTouch = false;
+        float moveDist = Vector3.Distance( inputStartPos , Input.mousePosition);
+        isXrot = 0;
+        if( moveDist < minMoveDist) {
+            Debug.Log("TOUCH EVENT");
+        }
+    }
+
+    void CheckMouseEvent(){
+        if (Input.GetMouseButtonDown(0)){
+            StartDrag();
+        }
+
+
+        if (Input.GetMouseButtonUp(0)){
+            EndDrag();
+        }
+
+
+        if ( startTouch == false ){
+            return;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            float moveDist = Vector3.Distance( inputStartPos , Input.mousePosition);
+            float moveX = inputStartPos.x - Input.mousePosition.x;
+            float moveY = inputStartPos.y - Input.mousePosition.y;
+            if( moveDist < minMoveDist) {
+               // Debug.Log("TOUCH EVENT");
+                return;
+            }
+
+            if( isXrot == 0 ) {
+                if( Mathf.Abs(moveX) > Mathf.Abs(moveY))
+                    isXrot = 1;
+                else
+                    isXrot = 2;
+            }
+
+
+
+
+            if( isXrot == 1 ){
+                Debug.Log( moveDist );
+                transform.rotation = Quaternion.Euler (new Vector3 (0, dragStartRotY + moveX / 4, 0));
+                if( Mathf.Abs(moveX / 4) > xFlipSnap) {
+                    //todo 여기서 tween 으로 90도의 배수가 되도록 회전시켜야함
+                    if( moveX > 0 )
+                        transform.rotation = Quaternion.Euler (new Vector3 (0, dragStartRotY + 90, 0));
+                    else
+                        transform.rotation = Quaternion.Euler (new Vector3 (0, dragStartRotY - 90, 0));
+                    EndDrag();
+                }
+            }
+            else {
+                float movePosY = dragStartPos.y - moveY / 8;
+                if ( movePosY > 0 )
+                    movePosY = 0;
+                transform.position = new Vector3( dragStartPos.x , movePosY, dragStartPos.z);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        CheckMouseEvent();
+
+       
+
+    }
+
+
+
+
+
+}
